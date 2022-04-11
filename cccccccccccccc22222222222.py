59c2ce61a7361af5e847011f8b18154ee91d7e16
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2022-04-11 09:04:30 -0400

    f

---
+++ cccccccccccccc22222222222.py
@@ -0,0 +1,124 @@
+
+
+import torch
+import operator
+import os
+import numpy as np
+
+import inception_pytorch
+
+
+
+
+model = torch.load('inception-2015-12-05.pt', map_location=torch.device('cpu'))
+std1 = model.state_dict()
+save_name = 'inception-2015-12-05.pdparams'
+model.eval()
+
+
+model2 = inception_pytorch.InceptionV3()
+std2 = model2.state_dict()
+model2.eval()
+
+
+map = {}
+already_used = []
+
+
+
+for key2, value2 in std2.items():
+    if '.bn.num_batches_tracked' in key2:
+        continue
+    if '.bn.weight' in key2:
+        continue
+    if '.conv.weight' in key2:
+        key1 = key2.replace('.conv.weight', '.weight')
+        map[key2] = key1
+        std2[key2] = std1[key1]
+    if '.bn.bias' in key2:
+        key1 = key2.replace('.bn.bias', '.beta')
+        map[key2] = key1
+        std2[key2] = std1[key1]
+    if '.bn.running_mean' in key2:
+        key1 = key2.replace('.bn.running_mean', '.mean')
+        map[key2] = key1
+        std2[key2] = std1[key1]
+    if '.bn.running_var' in key2:
+        key1 = key2.replace('.bn.running_var', '.var')
+        map[key2] = key1
+        std2[key2] = std1[key1]
+map['output.weight'] = 'output.weight'
+map['output.bias'] = 'output.bias'
+
+std2['output.weight'] = std1['output.weight']
+std2['output.bias'] = std1['output.bias']
+
+model2.load_state_dict(std2)
+
+
+# x_shape = [4, 3, 512, 512]
+# images = torch.randn(x_shape)
+
+import cv2
+images = cv2.imread('../data/data42681/afhq/train/cat/flickr_cat_000005.jpg')
+images = torch.Tensor(images)
+images = images.unsqueeze(0)
+images = images.permute(0, 3, 1, 2)
+
+
+
+def transform(img):
+    batch_size, channels, height, width, = img.shape
+    x = paddle.cast(img, paddle.float32)
+    theta = paddle.eye(2, 3, dtype=paddle.float32)
+    _3 = theta[0][2]
+    _4 = theta[0][0]
+    _5 = _4 / width
+    _6 = theta[0][0]
+    _7 = _3 + (_5 - _6 / 299.0)
+    _8 = theta[1][2]
+    _9 = theta[1][1]
+    _10 = (_9 / height)
+    _11 = theta[1][1]
+    _12 = _8 + (_10 - (_11 / 299.0))
+    _13 = paddle.unsqueeze(paddle.cast(theta, x.dtype), 0)
+    theta0 = paddle.tile(_13, [batch_size, 1, 1])
+    grid = paddle.nn.functional.affine_grid(theta0, [batch_size, channels, 299, 299], False, )
+    x0 = paddle.nn.functional.grid_sample(x, grid, "bilinear", "border", False, )
+    x1 = x0 - 128.0
+    x2 = x1 / 128.0
+    return x2
+
+
+return_features = False
+return_features = True
+
+use_fp16 = False
+# use_fp16 = True
+
+no_output_bias = False
+# no_output_bias = True
+
+code = model.code
+print(code)
+code = code.replace('_15 = features', '_15 = theta')
+print(code)
+model.code = code
+
+features = model(images, return_features=return_features, use_fp16=use_fp16, no_output_bias=no_output_bias)
+features2 = model2(images, return_features=return_features, use_fp16=use_fp16, no_output_bias=no_output_bias)
+
+# features = model.layers(images)
+# features2 = model2.layers(images)
+
+
+ddd = np.sum((features2.cpu().detach().numpy() - features.cpu().detach().numpy()) ** 2)
+print('diff=%.6f (%s)' % (ddd, 'features'))
+
+# ddd = np.sum((features3.cpu().detach().numpy() - features.cpu().detach().numpy()) ** 2)
+# print('diff=%.6f (%s)' % (ddd, 'features'))
+
+print()
+
+
+
--- inception_pytorch.py
+++ inception_pytorch.py
@@ -1,126 +1,15 @@
-import paddle.nn.functional
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import operator
 import os
 import numpy as np
-import torch.Tensor as Tensor
 
 
 
-
-model = torch.load('inception-2015-12-05.pt', map_location=torch.device('cpu'))
-std1 = model.state_dict()
-save_name = 'inception-2015-12-05.pdparams'
-
-
-
-
-class InceptionE(nn.Module):
-    def __init__(self,
-        in_features,
-    ):
-        super().__init__()
-        self.activation = in_features
-
-    def forward(self,
-                x: Tensor) -> Tensor:
-        a = (self.tower_conv).forward(x, )
-        b = (self.tower_1_conv_1).forward((self.tower_1_conv).forward(x, ), )
-        _0 = torch.contiguous((self.conv).forward(x, ))
-        _1 = torch.contiguous((self.tower_mixed_conv).forward(a, ))
-        _2 = (self.tower_mixed_conv_1).forward(a, )
-        _3 = torch.contiguous(_2)
-        _4 = (self.tower_1_mixed_conv).forward(b, )
-        _5 = torch.contiguous(_4)
-        _6 = (self.tower_1_mixed_conv_1).forward(b, )
-        _7 = torch.contiguous(_6)
-        _8 = (self.tower_2_conv).forward((self.tower_2_pool).forward(x, ), )
-        _9 = [_0, _1, _3, _5, _7, torch.contiguous(_8)]
-        return torch.cat(_9, 1)
-
-
-
-
-class InceptionD(nn.Module):
-    def __init__(self,
-        in_features,
-    ):
-        super().__init__()
-        self.activation = in_features
-
-    def forward(self,
-                x: Tensor) -> Tensor:
-        _0 = torch.contiguous((self.tower).forward(x, ))
-        _1 = torch.contiguous((self.tower_1).forward(x, ))
-        _2 = torch.contiguous((self.pool).forward(x, ))
-        return torch.cat([_0, _1, _2], 1)
-
-
-
-
-class InceptionC(nn.Module):
-    def __init__(self,
-        in_features,
-    ):
-        super().__init__()
-        self.activation = in_features
-
-    def forward(self,
-                x: Tensor) -> Tensor:
-        _0 = torch.contiguous((self.conv).forward(x, ))
-        _1 = torch.contiguous((self.tower).forward(x, ))
-        _2 = torch.contiguous((self.tower_1).forward(x, ))
-        _3 = torch.contiguous((self.tower_2).forward(x, ))
-        return torch.cat([_0, _1, _2, _3], 1)
-
-
-
-
-class InceptionB(nn.Module):
-    def __init__(self,
-        in_features,
-    ):
-        super().__init__()
-        self.activation = in_features
-
-    def forward(self,
-                x: Tensor) -> Tensor:
-        _0 = torch.contiguous((self.conv).forward(x, ))
-        _1 = torch.contiguous((self.tower).forward(x, ))
-        _2 = torch.contiguous((self.pool).forward(x, ))
-        return torch.cat([_0, _1, _2], 1)
-
-
-
-
-class InceptionA(nn.Module):
-    def __init__(self, num_channels, pool_features):
-        super().__init__()
-        self.conv = Conv2dLayer(num_channels, 64, 1)
-        self.tower = nn.Sequential()
-        self.tower.add_module('conv', Conv2dLayer(num_channels, 48, 1))
-        self.tower.add_module('conv_1', Conv2dLayer(48, 64, 5, padding=2))
-        self.tower_1 = nn.Sequential()
-        self.tower_1.add_module('conv', Conv2dLayer(num_channels, 64, 1))
-        self.tower_1.add_module('conv_1', Conv2dLayer(64, 96, 3, padding=1))
-        self.tower_1.add_module('conv_2', Conv2dLayer(96, 96, 3, padding=1))
-        self.tower_2 = nn.Sequential()
-        self.tower_2.add_module('pool', nn.AvgPool2d(kernel_size=3, stride=1, padding=1))
-        self.tower_2.add_module('conv', Conv2dLayer(num_channels, pool_features, 1))
-
-    def forward(self, x):
-        _0 = self.conv(x)
-        _1 = self.tower(x)
-        _2 = self.tower_1(x)
-        _3 = self.tower_2(x)
-        return torch.cat([_0, _1, _2, _3], 1)
-
-
-
-
-
+# model = torch.load('inception-2015-12-05.pt', map_location=torch.device('cpu'))
+# std1 = model.state_dict()
+# save_name = 'inception-2015-12-05.pdparams'
 
 
 
@@ -154,58 +43,134 @@ class Conv2dLayer(nn.Module):
 
 
 
-class Layers_Sequential(nn.Module):
-    def __init__(self,
-        in_features,                # Number of input features.
-        out_features,               # Number of output features.
-        bias            = True,     # Apply additive bias before the activation function?
-        activation      = 'linear', # Activation function: 'relu', 'lrelu', etc.
-        lr_multiplier   = 1,        # Learning rate multiplier.
-        bias_init       = 0,        # Initial value for the additive bias.
-    ):
-        super().__init__()
-        self.activation = activation
 
-    def forward(self,
-                input: Tensor) -> Tensor:
-        _0 = self.conv
-        _1 = self.conv_1
-        _2 = self.conv_2
-        _3 = self.pool0
-        _4 = self.conv_3
-        _5 = self.conv_4
-        _6 = self.pool1
-        _7 = self.mixed
-        _8 = self.mixed_1
-        _9 = self.mixed_2
-        _10 = self.mixed_3
-        _11 = self.mixed_4
-        _12 = self.mixed_5
-        _13 = self.mixed_6
-        _14 = self.mixed_7
-        _15 = self.mixed_8
-        _16 = self.mixed_9
-        _17 = self.mixed_10
-        _18 = self.pool2
-        input0 = (_0).forward(input, )
-        input1 = (_1).forward(input0, )
-        input2 = (_2).forward(input1, )
-        input3 = (_3).forward(input2, )
-        input4 = (_4).forward(input3, )
-        input5 = (_5).forward(input4, )
-        input6 = (_6).forward(input5, )
-        input7 = (_7).forward(input6, )
-        input8 = (_8).forward(input7, )
-        input9 = (_9).forward(input8, )
-        input10 = (_10).forward(input9, )
-        input11 = (_11).forward(input10, )
-        input12 = (_12).forward(input11, )
-        input13 = (_13).forward(input12, )
-        input14 = (_14).forward(input13, )
-        input15 = (_15).forward(input14, )
-        input16 = (_16).forward(input15, )
-        input17 = (_17).forward(input16, )
-        return (_18).forward(input17, )
+class InceptionE(nn.Module):
+    def __init__(self, num_channels, pool='avg'):
+        super().__init__()
+        assert pool in ['avg', 'max']
+        self.tower_conv = Conv2dLayer(num_channels, 384, 1)
+        self.tower_1_conv = Conv2dLayer(num_channels, 448, 1)
+        self.tower_1_conv_1 = Conv2dLayer(448, 384, 3, padding=1)
+        self.conv = Conv2dLayer(num_channels, 320, 1)
+        self.tower_mixed_conv = Conv2dLayer(384, 384, (1, 3), padding=(0, 1))
+        self.tower_mixed_conv_1 = Conv2dLayer(384, 384, (3, 1), padding=(1, 0))
+        self.tower_1_mixed_conv = Conv2dLayer(384, 384, (1, 3), padding=(0, 1))
+        self.tower_1_mixed_conv_1 = Conv2dLayer(384, 384, (3, 1), padding=(1, 0))
+        if pool == 'avg':
+            self.tower_2_pool = nn.AvgPool2d(kernel_size=3, stride=1, padding=1, count_include_pad=False)
+        elif pool == 'max':
+            self.tower_2_pool = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.tower_2_conv = Conv2dLayer(num_channels, 192, 1)
+
+    def forward(self, x):
+        a = self.tower_conv(x)
+        b = self.tower_1_conv_1(self.tower_1_conv(x))
+        _0 = self.conv(x)
+        _1 = self.tower_mixed_conv(a)
+        _2 = self.tower_mixed_conv_1(a)
+
+        _3 = _2
+        _4 = self.tower_1_mixed_conv(b)
+        _5 = _4
+        _6 = self.tower_1_mixed_conv_1(b)
+        _7 = _6
+        _8 = self.tower_2_conv(self.tower_2_pool(x))
+
+        _9 = [_0, _1, _3, _5, _7, _8]
+        return torch.cat(_9, 1)
+
+
+
+
+class InceptionD(nn.Module):
+    def __init__(self, num_channels):
+        super().__init__()
+        self.tower = nn.Sequential()
+        self.tower.add_module('conv', Conv2dLayer(num_channels, 192, 1))
+        self.tower.add_module('conv_1', Conv2dLayer(192, 320, 3, stride=2))
+        self.tower_1 = nn.Sequential()
+        self.tower_1.add_module('conv', Conv2dLayer(num_channels, 192, 1))
+        self.tower_1.add_module('conv_1', Conv2dLayer(192, 192, (1, 7), padding=(0, 3)))
+        self.tower_1.add_module('conv_2', Conv2dLayer(192, 192, (7, 1), padding=(3, 0)))
+        self.tower_1.add_module('conv_3', Conv2dLayer(192, 192, 3, stride=2))
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
+
+    def forward(self, x):
+        _0 = self.tower(x)
+        _1 = self.tower_1(x)
+        _2 = self.pool(x)
+        return torch.cat([_0, _1, _2], 1)
+
+
+
+
+class InceptionC(nn.Module):
+    def __init__(self, num_channels, channels_7x7):
+        super().__init__()
+        self.conv = Conv2dLayer(num_channels, 192, 1)
+        self.tower = nn.Sequential()
+        self.tower.add_module('conv', Conv2dLayer(num_channels, channels_7x7, 1, stride=1))
+        self.tower.add_module('conv_1', Conv2dLayer(channels_7x7, channels_7x7, (1, 7), stride=1, padding=(0, 3)))
+        self.tower.add_module('conv_2', Conv2dLayer(channels_7x7, 192, (7, 1), stride=1, padding=(3, 0)))
+        self.tower_1 = nn.Sequential()
+        self.tower_1.add_module('conv', Conv2dLayer(num_channels, channels_7x7, 1))
+        self.tower_1.add_module('conv_1', Conv2dLayer(channels_7x7, channels_7x7, (7, 1), padding=(3, 0)))
+        self.tower_1.add_module('conv_2', Conv2dLayer(channels_7x7, channels_7x7, (1, 7), padding=(0, 3)))
+        self.tower_1.add_module('conv_3', Conv2dLayer(channels_7x7, channels_7x7, (7, 1), padding=(3, 0)))
+        self.tower_1.add_module('conv_4', Conv2dLayer(channels_7x7, 192, (1, 7), padding=(0, 3)))
+        self.tower_2 = nn.Sequential()
+        self.tower_2.add_module('pool', nn.AvgPool2d(kernel_size=3, stride=1, padding=1, count_include_pad=False))
+        self.tower_2.add_module('conv', Conv2dLayer(num_channels, 192, 1))
+
+    def forward(self, x):
+        _0 = self.conv(x)
+        _1 = self.tower(x)
+        _2 = self.tower_1(x)
+        _3 = self.tower_2(x)
+        return torch.cat([_0, _1, _2, _3], 1)
+
+
+class InceptionB(nn.Module):
+    def __init__(self, num_channels):
+        super().__init__()
+        self.conv = Conv2dLayer(num_channels, 384, 3, stride=2)
+        self.tower = nn.Sequential()
+        self.tower.add_module('conv', Conv2dLayer(num_channels, 64, 1))
+        self.tower.add_module('conv_1', Conv2dLayer(64, 96, 3, padding=1))
+        self.tower.add_module('conv_2', Conv2dLayer(96, 96, 3, stride=2))
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
+
+    def forward(self, x):
+        _0 = self.conv(x)
+        _1 = self.tower(x)
+        _2 = self.pool(x)
+        return torch.cat([_0, _1, _2], 1)
+
+
+
+
+class InceptionA(nn.Module):
+    def __init__(self, num_channels, pool_features):
+        super().__init__()
+        self.conv = Conv2dLayer(num_channels, 64, 1)
+        self.tower = nn.Sequential()
+        self.tower.add_module('conv', Conv2dLayer(num_channels, 48, 1))
+        self.tower.add_module('conv_1', Conv2dLayer(48, 64, 5, padding=2))
+        self.tower_1 = nn.Sequential()
+        self.tower_1.add_module('conv', Conv2dLayer(num_channels, 64, 1))
+        self.tower_1.add_module('conv_1', Conv2dLayer(64, 96, 3, padding=1))
+        self.tower_1.add_module('conv_2', Conv2dLayer(96, 96, 3, padding=1))
+        self.tower_2 = nn.Sequential()
+        self.tower_2.add_module('pool', nn.AvgPool2d(kernel_size=3, stride=1, padding=1, count_include_pad=False))
+        self.tower_2.add_module('conv', Conv2dLayer(num_channels, pool_features, 1))
+
+    def forward(self, x):
+        _0 = self.conv(x)
+        _1 = self.tower(x)
+        _2 = self.tower_1(x)
+        _3 = self.tower_2(x)
+        return torch.cat([_0, _1, _2, _3], 1)
+
 
 
 
@@ -223,18 +188,25 @@ class InceptionV3(nn.Module):
         self.layers.add_module('mixed', InceptionA(192, 32))
         self.layers.add_module('mixed_1', InceptionA(256, 64))
         self.layers.add_module('mixed_2', InceptionA(288, 64))
-        self.layers.add_module('mixed_3', InceptionB(xxxxxxx))
+        self.layers.add_module('mixed_3', InceptionB(288))
+        self.layers.add_module('mixed_4', InceptionC(768, 128))
+        self.layers.add_module('mixed_5', InceptionC(768, 160))
+        self.layers.add_module('mixed_6', InceptionC(768, 160))
+        self.layers.add_module('mixed_7', InceptionC(768, 192))
+        self.layers.add_module('mixed_8', InceptionD(768))
+        self.layers.add_module('mixed_9', InceptionE(1280, pool='avg'))
+        self.layers.add_module('mixed_10', InceptionE(2048, pool='max'))
         self.layers.add_module('pool2', nn.AvgPool2d(kernel_size=8, stride=8, padding=0))
         self.output = nn.Linear(2048, class_num)
 
-    def forward(self,
-                img: Tensor,
-                return_features: bool = False,
-                use_fp16: bool = False,
-                no_output_bias: bool = False) -> Tensor:
+    def forward(self, img, return_features=False, use_fp16=False, no_output_bias=False):
         batch_size, channels, height, width, = img.shape
-        x = img.to(torch.float32)
-        theta = torch.eye(2, 3, dtype=torch.float32)
+        if use_fp16:
+            type_ = torch.float16
+        else:
+            type_ = torch.float32
+        x = img.to(type_)
+        theta = torch.eye(2, 3, dtype=None, device=x.device)
         _3 = torch.select(torch.select(theta, 0, 0), 0, 2)
         _4 = torch.select(torch.select(theta, 0, 0), 0, 0)
         _5 = torch.div(_4, width)
@@ -247,24 +219,23 @@ class InceptionV3(nn.Module):
         _12 = _8 + torch.sub(_10, torch.div(_11, 299))
         _13 = torch.unsqueeze(theta.to(x.dtype), 0)
         theta0 = _13.repeat([batch_size, 1, 1])
-        grid = torch.nn.functional.affine_grid(theta0, [batch_size, channels, 299, 299], False, )
-        x0 = torch.nn.functional.grid_sample(x, grid, "bilinear", "border", False, )
-        x1 = x0 - 128.0
-        x2 = x1 / 128.0
+        grid = torch.nn.functional.affine_grid(theta0, [batch_size, channels, 299, 299], False)
+        x0 = torch.nn.functional.grid_sample(x, grid, "bilinear", "border", False)
+        x1 = x0 - 127.5
+        x2 = x1 / 127.5
 
         _14 = torch.reshape(self.layers(x2), [-1, 2048])
-        # features = torch.to(_14, 6)
+        features = _14.to(torch.float32)
         if return_features:
-            # _15 = features
-            _15 = _14
-        # else:
-        #     if no_output_bias:
-        #         logits0 = __torch__.torch.nn.functional.linear(features, self.output.weight, None, )
-        #         logits = logits0
-        #     else:
-        #         logits = (self.output).forward(features, )
-        #     _16 = __torch__.torch.nn.functional.softmax(logits, 1, 3, None, )
-        #     _15 = _16
+            return _14
+        else:
+            if no_output_bias:
+                logits0 = torch.nn.functional.linear(features, self.output.weight, None, )
+                logits = logits0
+            else:
+                logits = self.output(features)
+            _16 = torch.nn.functional.softmax(logits, 1, 3, None, )
+            _15 = _16
         return _15
 
 
